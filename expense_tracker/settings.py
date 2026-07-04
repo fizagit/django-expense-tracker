b7316f361955e438d6e1b42402f677e1a7ccc527
fizagit <fizafathima441@gmail.com>
2026-07-05 02:38:49 +0530

    Fix static files for deployment

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY",
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://django-expense-tracker-yhrd.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
