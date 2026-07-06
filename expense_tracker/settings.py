fcf1ebb180855a86b9e4b456e577da3d367bdaa2
fizagit <fizafathima441@gmail.com>
2026-07-07 00:32:34 +0530

    updated settings.py

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY",
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["django-expense-tracker-yhrd.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost","django-expense-tracker-yhrd.onrender.com"]
 
 
 # Application definition
